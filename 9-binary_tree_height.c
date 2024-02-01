15517624adc1ddd9660f11074269c5a3f95a20d6
oswealth <osbertjaywealth@gmail.com>
2024-01-31 19:33:35 -0800

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,14 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    /* Return the maximum height of the left or right subtree, plus 1 for the current node */
-    return (1 + (left_height > right_height ? left_height : right_height));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
