f6384be6265fdc1e133451af2de6512171a6743b
amofasandra7@gmail.com <amofasandra7@gmail.com>
2024-02-02 23:20:20 +0000

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -51,8 +51,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	else
+	{
+		if (tree)
+		{
+			left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+		return ((left_height > right_height) ? left_height : right_height);
+	}
 }
